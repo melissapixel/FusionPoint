6ca633a364d5408f1ea0d7d5f170b8ba45bdd5c0
Mel <melissapixell@gmail.com>
2026-02-03 22:27:45 +0300

    feat(config): add company data file
    Introduce company.php to store centralized company information
    such as name, address, and contact details. This enables easy
    maintenance and consistent display across the application.

---
+++ config/company.php
@@ -0,0 +1,35 @@
+<?php
+return [
+    'name' => 'FusionPoint LLC',
+    'email' => 'info@rogaikopyta.ru',
+    'phone' => '+7 (495) 123-45-67',
+    'address' => '1 Primernaya St., Moscow',
+    'year_founded' => 1990,
+    'website' => 'https://rogaikopyta.ru',
+    'tax_id' => '7701234567', // INN (Russian Tax Identification Number)
+    'registration_number' => '1234567890123', // OGRN (Main State Registration Number)
+    'bank_details' => [
+        'bank_name' => 'JSC "Example Bank"',
+        'bik' => '044525225', // BIC (Bank Identification Code)
+        'correspondent_account' => '30101810400000000225',
+        'checking_account' => '40702810900000012345',
+    ],
+    'ceo' => 'Ivanov Ivan Ivanovich',
+    'accountant' => 'Petrov Petr Petrovich',
+    'legal_form' => 'Limited Liability Company',
+    'industry' => 'Retail / Trading',
+    'number_of_employees' => 150,
+    'core_values' => [
+        'Quality',
+        'Reliability',
+        'Innovation',
+        'Responsibility',
+        'Customer-Centric Approach',
+    ],
+    'social_media' => [
+        'facebook' => 'https://facebook.com',
+        'twitter' => 'https://twitter.com',
+        'linkedin' => 'https://linkedin.com',
+        'instagram' => 'https://instagram.com',
+    ],
+];
